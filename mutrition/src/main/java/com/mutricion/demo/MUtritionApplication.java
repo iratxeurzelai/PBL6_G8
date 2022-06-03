440b34522cc4832fc275ddbf02e108a5d49d2ed1
jonzurutuza <jonzurutuza18@gmail.com>
2022-06-03 10:32:22 +0200

    threads config

--- mutrition/src/main/java/com/mutricion/demo/MUtritionApplication.java
+++ mutrition/src/main/java/com/mutricion/demo/MUtritionApplication.java
@@ -2,8 +2,10 @@ package com.mutricion.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MUtritionApplication {
 
 	public static void main(String[] args) {
